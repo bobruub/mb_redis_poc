63109a915078d15912c9b7faaf235e12b0460ff7
bobruub <bobruub@users.noreply.github.com>
2022-04-20 14:29:51 +1000

    poc files

---
+++ start_mountebank.cmd
@@ -0,0 +1,1 @@
+C:\_git\perf_sven_statement_enrichment\mb.cmd --configfile C:\_git\perf_sven_statement_enrichment\imposters.ejs --allowInjection --port 12003 --logfile c:\var\logs\mb_account_api.log
